ac9f86f5c760a2d28d601c50d4d4780fee8f820b
Nuko <nuko973663@gmail.com>
2022-05-13 19:49:35 +0900

    update

--- src/Modal.js
+++ src/Modal.js
@@ -8,7 +8,7 @@ import {
   ModalBody,
   ModalCloseButton,
   Button,
-  Text
+  Text,
 } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react";
 import { useWeb3React } from "@web3-react/core";
@@ -28,31 +28,11 @@ export default function SelectWalletModal({ isOpen, closeModal }) {
         <ModalHeader>Select Wallet</ModalHeader>
         <ModalCloseButton
           _focus={{
-            boxShadow: "none"
+            boxShadow: "none",
           }}
         />
         <ModalBody paddingBottom="1.5rem">
           <VStack>
-            <Button
-              variant="outline"
-              onClick={() => {
-                activate(connectors.coinbaseWallet);
-                setProvider("coinbaseWallet");
-                closeModal();
-              }}
-              w="100%"
-            >
-              <HStack w="100%" justifyContent="center">
-                <Image
-                  src="/cbw.png"
-                  alt="Coinbase Wallet Logo"
-                  width={25}
-                  height={25}
-                  borderRadius="3px"
-                />
-                <Text>Coinbase Wallet</Text>
-              </HStack>
-            </Button>
             <Button
               variant="outline"
               onClick={() => {
--- src/connectors.js
+++ src/connectors.js
@@ -1,24 +1,19 @@
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 import { WalletLinkConnector } from "@web3-react/walletlink-connector";
+import { GnosisSafe } from "@web3-react/gnosis-safe";
 
 const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42]
+  supportedChainIds: [1, 3, 4, 5, 42],
 });
 
 const walletconnect = new WalletConnectConnector({
   rpcUrl: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
   bridge: "https://bridge.walletconnect.org",
-  qrcode: true
-});
-
-const walletlink = new WalletLinkConnector({
-  url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
-  appName: "web3-react-demo"
+  qrcode: true,
 });
 
 export const connectors = {
   injected: injected,
   walletConnect: walletconnect,
-  coinbaseWallet: walletlink
 };
